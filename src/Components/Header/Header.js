fac4c5bf84aa7c8cc08813018f4744aa4f227495
thoover1 <thoover1@uab.edu>
2020-01-14 13:29:19 -0600

    header design done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,19 +8,37 @@ export default class Header extends Component {
     return (
       <div className="header-main">
         <div className="header-contents">
-          <div className="header-left">
-            <NavLink to="/">
-              <img className="header-logo" src={logo} alt="logo" />
-            </NavLink>
+          <div className="header-top-contents">
+            <span className="header-left">
+              <NavLink to="/">
+                <img className="header-logo" src={logo} alt="logo" />
+              </NavLink>
+            </span>
+            <nav className="header-right">
+              <ul>
+                <p>
+                  Call Now: <a href="tel:3077608328">(307)760-8328</a>
+                </p>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About</li>
+                <li className="contact-nav">Email</li>
+                {/* <i class="fas fa-shopping-cart"></i> */}
+              </ul>
+            </nav>
           </div>
-          <span className="header-right">
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-              {/* <i class="fas fa-shopping-cart"></i> */}
-            </ul>
-          </span>
+          <div className="header-middle-contents">
+            <span className="middle-contents">
+              <h1>Rodeo Turtle, LLC</h1>
+              <br />
+              <br />
+              <br />
+              <h3>Premier Custom Metal Design</h3>
+              {/* <h3>&</h3> */}
+              <br />
+              <h3>Patent Designed Turtle Spurs</h3>
+            </span>
+          </div>
         </div>
       </div>
     );
