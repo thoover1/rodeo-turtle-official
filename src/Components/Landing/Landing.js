c7722479789ceb38806c06fd557453b8f368c868
thoover1 <thoover1@uab.edu>
2020-01-20 15:24:42 -0600

    added landing about section

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -5,8 +5,32 @@ import "./Landing.scss";
 export default function Landing() {
   return (
     <div className="landing-page">
-      <img src={landingBackground} className="landing-background"></img>
-      <h1>HOWDY HOWDY LETS GET ROWDY!</h1>
+      <div className="landing-main">
+        <img src={landingBackground}></img>
+        <h2>Rodeo Turtle, LLC</h2>
+        {/* <h1>HOWDY HOWDY LETS GET ROWDY!</h1> */}
+      </div>
+      <div className="landing-about">
+        <section>
+          <h3>About Us</h3>
+          <p>
+            We like rodeos and turtles and metal and wood and trucks and beer
+            other stuff that I'm just trying to fill the page with. We have dogs
+            and a newborn. We are here to serve you. My turtles spurs are the
+            bomb and you know you want them. Enough words now. Buy my shit.
+          </p>
+          {/* <br /> */}
+          <p>
+            We like rodeos and turtles and metal and wood and trucks and beer
+            other stuff that I'm just trying to fill the page with. We have dogs
+            and a newborn. We are here to serve you. My turtles spurs are the
+            bomb and you know you want them. Enough words now. Buy my shit.
+          </p>
+
+          <a href="/about">Learn more</a>
+        </section>
+        <img src="http://www.indywrestlers.net/indynet/wp-content/uploads/2011/06/Johnny-Meadows.jpg" />
+      </div>
     </div>
   );
 }
