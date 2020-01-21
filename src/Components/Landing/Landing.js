32c4d771e5a3cc3067a73ed628e57acd75263bc0
thoover1 <thoover1@uab.edu>
2020-01-20 23:49:02 -0600

    added landing gallery

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -26,10 +26,21 @@ export default function Landing() {
             and a newborn. We are here to serve you. My turtles spurs are the
             bomb and you know you want them. Enough words now. Buy my shit.
           </p>
-
           <a href="/about">Learn more</a>
         </section>
         <img src="http://www.indywrestlers.net/indynet/wp-content/uploads/2011/06/Johnny-Meadows.jpg" />
+      </div>
+      {/* <hr/> */}
+      <div className="landing-gallery">
+        <h1>Gallery</h1>
+        <section>
+          <img src="https://images.pexels.com/photos/1094767/pexels-photo-1094767.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+          <img src="https://images.pexels.com/photos/175709/pexels-photo-175709.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <img src="https://images.pexels.com/photos/313776/pexels-photo-313776.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <img src="https://images.pexels.com/photos/227577/pexels-photo-227577.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <img src="https://images.pexels.com/photos/1094770/pexels-photo-1094770.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <img src="https://images.pexels.com/photos/313773/pexels-photo-313773.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        </section>
       </div>
     </div>
   );
