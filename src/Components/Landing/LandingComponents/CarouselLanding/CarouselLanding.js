3061408d4c3ee07ef8ca216a6c650e0264e4a04d
thoover1 <thoover1@uab.edu>
2020-03-12 14:29:27 -0500

    maybe this will work

--- src/Components/Landing/LandingComponents/CarouselLanding/CarouselLanding.js
+++ src/Components/Landing/LandingComponents/CarouselLanding/CarouselLanding.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import landingBackground from "../../../../assets/Plasma-Cutter_5";
 import landingBackground2 from "../../../../assets/landingBackground2.jpg";
 import Carousel, { Dots } from "@brainhubeu/react-carousel";
-import "@brainhubeu/react-carousel/lib/style.css";
+// import "@brainhubeu/react-carousel/lib/style.css";
 import "./CarouselLanding.scss";
 
 export default function CarouselLanding() {
@@ -12,12 +12,12 @@ export default function CarouselLanding() {
       autoPlay={10000}
       animationSpeed={2000}
       infinite
-      dots
       className="CarouselLanding"
     >
       <img className="CLImages" src={landingBackground} />
       <img className="CLImages" src={landingBackground2} />
       {/* <img src={imageThree} /> */}
+      {/* <Dots /> */}
     </Carousel>
     // <div className="CarouselLanding">
     //   <img ClassName="CLImages" src={landingBackground}></img>
