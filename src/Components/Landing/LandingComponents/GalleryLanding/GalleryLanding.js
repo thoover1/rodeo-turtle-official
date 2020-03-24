dc98ab8b69007a27e4ecda6d037e2b6803f5f24a
thoover1 <thoover1@uab.edu>
2020-03-24 15:53:49 -0500

    fixed styling and included plus icons for eventual modals

--- src/Components/Landing/LandingComponents/GalleryLanding/GalleryLanding.js
+++ src/Components/Landing/LandingComponents/GalleryLanding/GalleryLanding.js
@@ -7,18 +7,54 @@ export default function GalleryLanding() {
     <div className="landing-gallery">
       <h1>Gallery</h1>
       <section>
-        <img src="https://images.pexels.com/photos/1094767/pexels-photo-1094767.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-        <img src="https://images.pexels.com/photos/175709/pexels-photo-175709.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-        <img src="https://images.pexels.com/photos/313776/pexels-photo-313776.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-        <img src="https://images.pexels.com/photos/227577/pexels-photo-227577.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-        <img src="https://images.pexels.com/photos/1094770/pexels-photo-1094770.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-        <img src="https://images.pexels.com/photos/175709/pexels-photo-175709.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-        <img src="https://images.pexels.com/photos/1094767/pexels-photo-1094767.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-        <img src="https://images.pexels.com/photos/313773/pexels-photo-313773.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-        <img src="https://images.pexels.com/photos/1094767/pexels-photo-1094767.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-        <img src="https://images.pexels.com/photos/175709/pexels-photo-175709.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-        <img src="https://images.pexels.com/photos/313776/pexels-photo-313776.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-        <img src="https://images.pexels.com/photos/227577/pexels-photo-227577.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/1094767/pexels-photo-1094767.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/175709/pexels-photo-175709.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/313776/pexels-photo-313776.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/227577/pexels-photo-227577.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/1094770/pexels-photo-1094770.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/175709/pexels-photo-175709.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/1094767/pexels-photo-1094767.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/313773/pexels-photo-313773.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/1094767/pexels-photo-1094767.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/175709/pexels-photo-175709.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/313776/pexels-photo-313776.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="image-container">
+          <img src="https://images.pexels.com/photos/227577/pexels-photo-227577.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <i class="fas fa-plus"></i>
+        </div>
       </section>
       <NavLink to="/gallery">
         <p>More From Our Gallery</p>
