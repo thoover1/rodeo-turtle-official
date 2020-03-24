2bdc19a639ac295eac1f32ee5dba161def142100
thoover1 <thoover1@uab.edu>
2020-03-24 16:50:06 -0500

    navigation items done for now but still needs more responsive styling

--- src/Components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/Components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./NavigationItem.css";
+import "./NavigationItem.scss";
 
 const navigationItem = props => (
   <li className="NavigationItem">
--- src/Components/Navigation/NavigationItems/NavigationItems.js
+++ src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavigationItem from "./NavigationItem/NavigationItem";
-import "./NavigationItems.css";
+import "./NavigationItems.scss";
 
 const navigationItems = props => (
   <ul className="NavigationItems">
