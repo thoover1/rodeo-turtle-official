18b6890bca6e1a09336f6ac44914a7ba35dd2234
thoover1 <thoover1@uab.edu>
2020-03-25 17:18:33 -0500

    unhappy with carousellanding need to completely rework into slideshow?

--- src/Components/Landing/LandingComponents/CarouselLanding/CarouselLanding.js
+++ src/Components/Landing/LandingComponents/CarouselLanding/CarouselLanding.js
@@ -3,31 +3,31 @@ import landingBackground from "../../../../assets/Plasma-Cutter_5";
 import landingBackground2 from "../../../../assets/landingBackground2.jpg";
 import landingBackground3 from "../../../../assets/landingBackground3.jpg";
 import landingBackground4 from "../../../../assets/landingBackground4.jpg";
-import Carousel, { Dots } from "@brainhubeu/react-carousel";
+// import Carousel, { Dots } from "@brainhubeu/react-carousel";
 // import "@brainhubeu/react-carousel/lib/style.css";
 import "./CarouselLanding.scss";
 
 export default function CarouselLanding() {
   return (
-    <Carousel
-      // arrows
-      autoPlay={5000}
-      animationSpeed={2000}
-      infinite
-      // stopAutoPlayOnHover
-      // dots
-      className="BrainhubCarousel"
-    >
-      <img className="CLImages" src={landingBackground} />
-      <img className="CLImages" src={landingBackground3} />
-      <img className="CLImages" src={landingBackground2} />
-      <img className="CLImages" src={landingBackground4} />
-      {/* <img src={imageThree} /> */}
-      {/* <Dots /> */}
-    </Carousel>
-    // <div className="CarouselLanding">
-    //   <img ClassName="CLImages" src={landingBackground}></img>
-    //   <img ClassName="CLImages" src={landingBackground2}></img>
-    // </div>
+    // <Carousel
+    //   // arrows
+    //   autoPlay={5000}
+    //   animationSpeed={2000}
+    //   infinite
+    //   // stopAutoPlayOnHover
+    //   // dots
+    //   className="BrainhubCarousel"
+    // >
+    //   <img className="CLImages" src={landingBackground} />
+    //   <img className="CLImages" src={landingBackground3} />
+    //   <img className="CLImages" src={landingBackground2} />
+    //   <img className="CLImages" src={landingBackground4} />
+    //   {/* <img src={imageThree} /> */}
+    //   {/* <Dots /> */}
+    // </Carousel>
+    <div className="CarouselLanding">
+      <img ClassName="CLImages" src={landingBackground}></img>
+      {/* <img ClassName="CLImages" src={landingBackground2}></img> */}
+    </div>
   );
 }
