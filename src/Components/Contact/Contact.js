3942f536fdc37d48f80a16041340b30148aee0c7
thoover1 <thoover1@uab.edu>
2020-03-26 18:25:43 -0500

    happy with contact page for now but still needs functionality to form submission

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,71 +1,49 @@
-import React from 'react'
-import './Contact.scss'
+import React from "react";
+import location from "../../assets/location.png";
+import "./Contact.scss";
 
 export default function Contact() {
-    return (
-        <div className='Contact'>
-            <p>contact111111111 page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
-            <p>contact page</p>
+  return (
+    <div className="Contact">
+      <div className="container">
+        <div className="contact-left">
+          <div className="contact-title">
+            <h1>Contact Us</h1>
+          </div>
+          <div className="contact-info-page">
+            <h3>
+              Thank you for your interest in Rodeo Turtle. Please provide your
+              contact details by completing this form and one of our
+              representatives will be in touch with you.
+            </h3>
+          </div>
+          <div className="contact-lines-page">
+            <h3>(xxx) xxx - xxxx</h3>
+            <h3>info@rodeoturtle.com</h3>
+          </div>
+          <div className="contact-hours-page">
+            <h2>Hours of Operation</h2>
+            <h3>Monday - Friday:</h3>
+            <h3>9:00AM - 5:00PM</h3>
+          </div>
+          <img src={location} />
         </div>
-    )
+        <div className="contact-right">
+          <form>
+            <input type="text" placeholder="Name" required />
+            <input type="email" placeholder="Email" required />
+            <input type="tel" placeholder="Phone Number (optional)" />
+            <input type="text" placeholder="Reason For Contacting" required />
+            <input
+              className="message-field"
+              type="text"
+              placeholder="Type Your Message Here..."
+              required
+            />
+            <button>Fire Away!</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
