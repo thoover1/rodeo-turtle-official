6b2f55e837bc7ccddb6556f2e726f8b460b4c5cc
thoover1 <thoover1@uab.edu>
2020-03-27 13:59:19 -0500

    put down info for future development

--- src/Components/GoodsNGear/GoodsNGear.js
+++ src/Components/GoodsNGear/GoodsNGear.js
@@ -1,74 +1,88 @@
-import React from 'react'
-import './GoodsNGear.scss'
+import React from "react";
+import "./GoodsNGear.scss";
 
 export default function GoodsNGear() {
-    return (
-        <div className='GoodsNGear'>
-            <p>Goods1111111111111 & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-            <p>Goods & Gear page</p>
-        </div>
-    )
+  return (
+    <div className="GoodsNGear">
+      <h1>
+        This is just filler content. The way this page is set up for now, this
+        space must be filled with something so that the footer doesn't do wonky
+        stuff.
+      </h1>
+      <h2>
+        In the future, this site will be easier to manage with a database that
+        pulls product inventory info (e.g. pictures, prices)
+      </h2>
+      <h3>
+        Additionally, once this page is fully functional, you will need a login
+        that has user info as well as a cart page.
+      </h3>
+      <h4>
+        Because there is nothing to sell yet (Goods & Gear wise), I just have
+        this page built in so it will be easier to pick back up whenever the
+        time comes for this stage in the business
+      </h4>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+      <p>Goods & Gear page</p>
+    </div>
+  );
 }
