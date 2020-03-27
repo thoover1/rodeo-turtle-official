e5fd7205ab81bb551dd542e069380c625b490963
thoover1 <thoover1@uab.edu>
2020-03-27 14:08:18 -0500

    better filler info

--- src/Components/GoodsNGear/GoodsNGear.js
+++ src/Components/GoodsNGear/GoodsNGear.js
@@ -22,67 +22,10 @@ export default function GoodsNGear() {
         this page built in so it will be easier to pick back up whenever the
         time comes for this stage in the business
       </h4>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
-      <p>Goods & Gear page</p>
+      <div>
+        <h1>Rodeo Turtle Coming At You Like</h1>
+        <img src="https://imgix.bustle.com/uploads/image/2018/2/20/5c56a0c6-b9eb-4057-942c-491ba048a7ff-3653334524001_5662721739001_5662718067001-vs.jpg" />
+      </div>
     </div>
   );
 }
