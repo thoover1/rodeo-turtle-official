b9a04e89ce8ceb6446dc57cbcd02e9a73e0102bd
thoover1 <thoover1@uab.edu>
2020-03-30 14:53:30 -0500

    added text animation for home of the turtle spur

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Landing.scss";
 import AboutLanding from "./LandingComponents/AboutLanding/AboutLanding";
 import GalleryLanding from "./LandingComponents/GalleryLanding/GalleryLanding";
 import CarouselLanding from "./LandingComponents/CarouselLanding/CarouselLanding";
+import Typist from "react-typist";
 
-// const title = 'Rodeo Turtle';
-// let i = 0;
+const Landing = () => {
+  const [count, setCount] = useState(true);
 
-export default function Landing() {
+    useEffect(() => {
+    setCount(true);
+  }, [count]);
+
   return (
     <div className="landing-page">
       <div className="landing-main">
         <CarouselLanding />
         <h2>Rodeo Turtle</h2>
+        {count ? (
+          // include 'onTypingDone={() => setCount(false)}' after avgTypingDelay to make infite typing loop
+        <Typist className='typing' avgTypingDelay={80} >
+          <span> Home of the Turtle Spur</span>
+          {/* include content below to edit text as an animation */}
+          {/* <Typist.Backspace count={11} delay={800} />
+          <span>Custom Metal Design</span>
+          <Typist.Backspace count={11} delay={800} />
+          <span>Custom Wood Design</span> */}
+        </Typist>
+      ) : (
+        ""
+      )}
         <i class="fas fa-arrow-down"></i>
       </div>
       <AboutLanding />
@@ -20,3 +37,5 @@ export default function Landing() {
     </div>
   );
 }
+
+export default Landing;
