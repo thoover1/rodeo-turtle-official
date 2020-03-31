e3de90b319a4903503b2792c8cc5c2ed2278eeea
thoover1 <thoover1@uab.edu>
2020-03-31 14:55:41 -0500

    altered timing of text to arrow for better flow on page load

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./Landing.scss";
 import AboutLanding from "./LandingComponents/AboutLanding/AboutLanding";
 import GalleryLanding from "./LandingComponents/GalleryLanding/GalleryLanding";
@@ -8,13 +8,13 @@ import Typist from "react-typist";
 const Landing = () => {
   const [count, setCount] = useState(0);
   const [arrow, setArrow] = useState(null);
-    useEffect(() => {
-      setTimeout(() => {
-        setCount(1)
-      }, 1000)
-       setTimeout(() => {
-        setArrow(<i class="fas fa-arrow-down"></i>)
-      }, 4000)
+  useEffect(() => {
+    setTimeout(() => {
+      setCount(1);
+    }, 1000);
+    setTimeout(() => {
+      setArrow(<i class="fas fa-arrow-down"></i>);
+    }, 4500);
     // setCount(true);
   }, [count]);
 
@@ -25,23 +25,27 @@ const Landing = () => {
         <h2>Rodeo Turtle</h2>
         {count ? (
           // include 'onTypingDone={() => setCount(false)}' after avgTypingDelay to make infite typing loop
-        <Typist className='typing' cursor={{hideWhenDone: true, hideWhenDoneDelay: -1000}}avgTypingDelay={80} >
-          <span> Home of the Turtle Spur</span>
-          {/* include content below to edit text as an animation */}
-          {/* <Typist.Backspace count={11} delay={800} />
+          <Typist
+            className="typing"
+            cursor={{ hideWhenDone: true, hideWhenDoneDelay: -1000 }}
+            avgTypingDelay={100}
+          >
+            <span> Home of the Turtle Spur</span>
+            {/* include content below to edit text as an animation */}
+            {/* <Typist.Backspace count={11} delay={800} />
           <span>Custom Metal Design</span>
           <Typist.Backspace count={11} delay={800} />
           <span>Custom Wood Design</span> */}
-        </Typist>
-      ) : (
-        ""
-      )}
-      {arrow}
+          </Typist>
+        ) : (
+          ""
+        )}
+        {arrow}
       </div>
       <AboutLanding />
       <GalleryLanding />
     </div>
   );
-}
+};
 
 export default Landing;
